969decb31562e4a0d359cf60a7340d133a2c4698
Kostya-Romanovskyi <romanovskiyk97@gmail.com>
2024-09-16 01:49:46 -0700

    fixed base name route

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -14,15 +14,15 @@ const queryClient = new QueryClient({
 			refetchOnWindowFocus: false,
 		},
 	},
-})
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<ReactQueryDevtools />
-			<BrowserRouter basename='/'>
+			<BrowserRouter basename='/own-shop/'>
 				<App />
 			</BrowserRouter>
 		</QueryClientProvider>
 	</React.StrictMode>
-)
+);
