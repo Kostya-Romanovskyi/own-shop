eee23d1763d0b12bcced69dea1f3a63ca14e6a8e
Konstantin Romanovskiy <konstantinromanovskiy@Konstantins-MacBook-Pro.local>
2025-02-24 22:15:57 -0800

    fixed notifications for new order

--- src/components/HeaderStaff/HeaderStaff.tsx
+++ src/components/HeaderStaff/HeaderStaff.tsx
@@ -1,27 +1,72 @@
 import { useEffect } from 'react';
 import { useGetAllOrders } from '../../hooks/useOrder';
+import { useAllReservations } from '../../hooks/useReservations';
 import { Link } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import LogoutBtn from '../LogoutBtn/LogoutBtn';
 import playOrderSound from '../../helpers/PlayOrderSound';
+import { useState } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
+import io from 'socket.io-client';
 import './header-staff.scss';
+
+const usePendingStatus = (items: any, statusType: string) => {
+  const [hasPending, setHasPending] = useState(false);
+
+  useEffect(() => {
+    setHasPending(items ? items.some((item: any) => item.status === statusType) : false);
+  }, [items, statusType]);
+
+  return hasPending;
+};
+
+// hook for sound
+const useSoundNotification = (shouldPlay: boolean, message: string) => {
+  useEffect(() => {
+    if (!shouldPlay) return;
+
+    playOrderSound();
+
+    const intervalId = setInterval(playOrderSound, 5000);
+
+    return () => clearInterval(intervalId);
+  }, [shouldPlay, message]);
+};
 
 const HeaderStaff = () => {
   const { data: orders } = useGetAllOrders();
+  const { data: reservations } = useAllReservations();
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    // const socket = io('http://localhost:3000');
+    const socket = io('https://own-shop-back.onrender.com');
+
+    socket.on('NEW_ORDER', newOrder => {
+      queryClient.invalidateQueries({ queryKey: ['AllOrders'] });
+      console.log('New order received:', newOrder);
+      playOrderSound();
+    });
+
+    socket.on('NEW_RESERVATION', newReservation => {
+      queryClient.invalidateQueries({ queryKey: ['all-reservations'] });
+      console.log('New reservation received:', newReservation);
+      playOrderSound();
+    });
+
+    return () => {
+      socket.off('NEW_ORDER');
+      socket.off('NEW_RESERVATION');
+      socket.disconnect();
+    };
+  }, []);
+
+  const hasPendingOrders = usePendingStatus(orders, 'Pending');
+  const hasPendingReservations = usePendingStatus(reservations, 'Pending');
+
+  useSoundNotification(hasPendingOrders, 'orders Pending!');
+  useSoundNotification(hasPendingReservations, 'reservations Pending!');
 
   const checkStatus = orders?.find(order => order.status === 'Pending');
-
-  useEffect(() => {
-    if (checkStatus?.status === 'Pending') {
-      playOrderSound();
-
-      const intervalId = setInterval(playOrderSound, 5000);
-
-      return () => clearInterval(intervalId);
-    }
-
-    return () => {};
-  }, [checkStatus]);
 
   return (
     <>
--- src/pages/Staff/Staff.tsx
+++ src/pages/Staff/Staff.tsx
@@ -1,54 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import Modal from '../../components/Modal/Modal';
 import StaffOrdersList from '../../components/StaffOrdersList/StaffOrdersList';
 import StaffReservationList from '../../components/StaffReservationList/StaffReservationList';
-import io from 'socket.io-client';
 import { useAllReservations } from '../../hooks/useReservations';
 import { useGetAllOrders } from '../../hooks/useOrder';
-import { useQueryClient } from '@tanstack/react-query';
-import playOrderSound from '../../helpers/PlayOrderSound';
 
 import './staff.scss';
-
-// hook for checking status
-const usePendingStatus = (items: any, statusType: string) => {
-  const [hasPending, setHasPending] = useState(false);
-
-  useEffect(() => {
-    setHasPending(items ? items.some((item: any) => item.status === statusType) : false);
-  }, [items, statusType]);
-
-  return hasPending;
-};
-
-// hook for sound
-const useSoundNotification = (shouldPlay: boolean, message: string) => {
-  useEffect(() => {
-    if (!shouldPlay) return;
-
-    playOrderSound();
-
-    const intervalId = setInterval(playOrderSound, 5000);
-
-    return () => clearInterval(intervalId);
-  }, [shouldPlay, message]);
-};
 
 const Staff = () => {
   const [isModalOpen, setIsModalOpen] = useState(true);
   const [searchParams, setSearchParams] = useSearchParams();
-  const queryClient = useQueryClient();
 
   const tab = searchParams.get('tab') || 'orders';
   const { data: orders, isPending } = useGetAllOrders();
   const { data: reservations } = useAllReservations();
-
-  const hasPendingOrders = usePendingStatus(orders, 'Pending');
-  const hasPendingReservations = usePendingStatus(reservations, 'Pending');
-
-  useSoundNotification(hasPendingOrders, 'orders Pending!');
-  useSoundNotification(hasPendingReservations, 'reservations Pending!');
 
   const newOrdersCount = orders
     ? orders.reduce((acc, order) => acc + (order.status === 'Pending' ? 1 : 0), 0)
@@ -56,29 +22,6 @@ const Staff = () => {
   const newReservationCount = reservations
     ? reservations.reduce((acc, res) => acc + (res.status === 'Pending' ? 1 : 0), 0)
     : 0;
-
-  useEffect(() => {
-    // const socket = io('http://localhost:3000');
-    const socket = io('https://own-shop-back.onrender.com');
-
-    socket.on('NEW_ORDER', newOrder => {
-      queryClient.invalidateQueries({ queryKey: ['AllOrders'] });
-      console.log('New order received:', newOrder);
-      playOrderSound();
-    });
-
-    socket.on('NEW_RESERVATION', newReservation => {
-      queryClient.invalidateQueries({ queryKey: ['all-reservations'] });
-      console.log('New reservation received:', newReservation);
-      playOrderSound();
-    });
-
-    return () => {
-      socket.off('NEW_ORDER');
-      socket.off('NEW_RESERVATION');
-      socket.disconnect();
-    };
-  }, []);
 
   return (
     <div className="staff__container">
